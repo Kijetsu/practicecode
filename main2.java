c6177668c2b332711025a2c739d650537ee23792
kaze <ralletarikharloey@gmail.com>
2026-07-25 20:10:53 +0800

    dog

---
+++ main2.java
@@ -0,0 +1,13 @@
+public class main2 {
+    public class main {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Hello World from dog A!");
+        System.out.println("Hello World from dog B!");
+        System.out.println("Hello World from dog C!");
+        
+    }
+    
+}
+}
